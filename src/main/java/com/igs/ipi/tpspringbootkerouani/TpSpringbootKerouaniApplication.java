60a98704be6690e93ece3a4211f4311f81c8ef99
Florian Kerouani <florian.kerouani@ipilyon.net>
2017-12-14 13:33:41 +0100

    Premier Commit

---
+++ src/main/java/com/igs/ipi/tpspringbootkerouani/TpSpringbootKerouaniApplication.java
@@ -0,0 +1,12 @@
+package com.igs.ipi.tpspringbootkerouani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringbootKerouaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringbootKerouaniApplication.class, args);
+	}
+}
---
+++ src/test/java/com/igs/ipi/tpspringbootkerouani/TpSpringbootKerouaniApplicationTests.java
@@ -0,0 +1,16 @@
+package com.igs.ipi.tpspringbootkerouani;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TpSpringbootKerouaniApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
